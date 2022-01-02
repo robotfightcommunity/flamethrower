51d0727b0f2c71530d8506739bda264806ab9f80
Pavel Lonkin <plonkin@odin.com>
2022-01-02 19:08:11 +0100

    Debug on real board, fix issues with ignition + fix readme

--- README.md
+++ README.md
@@ -8,3 +8,33 @@ It uses two PWM input to control:
 For more details you can reference to the youtube channel:
 
 https://www.youtube.com/channel/UCf2gzx_wVlQ8WtN4Uq27aKA
+
+
+# Features
+1. Support of safety switch. When safety swith is ON, firmare turn off the flame and ignition. Also it skips any input fire signals
+2. Support for fire switch
+
+# State Diagram
+![State Diagram](./docs/flamethrower_state_diagram.png)
+
+
+# Configuration Variables
+
+## Input/output pins
+1. PWM_PIN_SAFETY - safety pin number
+2. PWM_PIN_INPUT - servo input for controlling flame power pin number
+3. OUTPUT_SAFETY - safety highlight diods pin number
+4. OUTPUT_PIN_IGNITION - ignition control pin number
+
+## Flame and Ignition Variables
+1. IGNITION_DELAY - how long ignition should be turned on (milliseconds)
+2. IGNITION_SERVO_POSITION - position for the servo when ignition is in progress (degress)
+3. FLAME_SERVO_START - start position of servo (usually same as IGNITION_SERVO_POSITION) (degrees)
+4. FLAME_SERVO_END - end positions of servo (degrees)
+5. FLAME_SERVO_UP_STEP - step of servo for increasing the flame (degrees)
+6. FLAME_SERVO_DOWN_STEP - step of servo for decreasing the flame (degrees)
+7. FLAME_SERVO_UP_DELAY - how fast to move servo to upper position (milliseconds)
+8. FLAME_SERVO_DOWN_DELAY - how fast to move servo to lower position (milliseconds)
+
+# TODO
+1. Support of lost signal. When there is no signal for 5-10 seconds - turn safety ON, switch off flame
--- flamethrower.ino
+++ flamethrower.ino
@@ -1,111 +1,201 @@
-// input pin, that controls ignition
-#define PWM_PIN_IN_IGNITION 3
-// input pin controls power
-#define PWM_PIN_IN_POWER 2
+#include <Servo.h>
 
-// output pin ignition
-#define OUTPUT_PIN_IGNITION 8
+// input pin, that controls safety
+#define PWM_PIN_SAFETY 2
+// input pin controls whole process
+#define PWM_PIN_INPUT 3
+
+// output safety
+#define OUTPUT_SAFETY 7
 // output pin power
-#define OUTPUT_PIN_POWER 9
+#define OUTPUT_PIN_SERVO 9
+// output for ignition
+#define OUTPUT_PIN_IGNITION 10
 
 #define LOW_LOW_BOUNDARY 900
 #define LOW_HIGH_BOUNDARY 1100
 #define HIGH_LOW_BOUNDARY 1900
 #define HIGH_HIGH_BOUNDARY 2100
 
-// timeout on enable safety if signal was lost
-#define NO_SIGNAL_SAFETY_TIMEOUT 50000
+// ignition delay in milliseconds
+#define IGNITION_DELAY 500
+// ignition power in degrees of servo position
+#define IGNITION_SERVO_POSITION 5
 
-volatile unsigned long pwm_in_ignition_start = 0;
-volatile boolean pwm_in_ignition_new_throttle_signal = false;
-volatile int ignition_throttle_len = 0;
-volatile long last_signal_ignition_time = 0;
+#define FLAME_SERVO_START 5  // degrees
+#define FLAME_SERVO_END 25  // degrees
+#define FLAME_SERVO_UP_STEP 5  // degress
+#define FLAME_SERVO_DOWN_STEP 1  // degrees
+#define FLAME_SERVO_UP_DELAY 200  // milliseconds
+#define FLAME_SERVO_DOWN_DELAY 300  // milliseconds
 
-volatile unsigned long pwm_in_power_start = 0;
-volatile boolean pwm_in_power_new_throttle_signal = false;
-volatile int power_throttle_len = 0;
-volatile long last_signal_power_time = 0;
+// state machine positions
+#define INITIAL 0
+#define READY 1
+#define IGNITION 2
+#define MOVING_UP_FAST 3
+#define MOVING_DOWN_SLOW 4
 
-boolean was_ignition_pin_low = false;
+volatile unsigned long pwm_safety_start = 0;
+volatile boolean pwm_safety_throttle_signal = false;
+volatile unsigned int safety_throttle_len = 0;
+volatile unsigned long last_signal_safety_time = 0;
 
-long current_time = 0;
+volatile unsigned long pwm_input_start = 0;
+volatile boolean pwm_input_throttle_signal = false;
+volatile unsigned int input_throttle_len = 0;
+volatile unsigned long last_signal_input_time = 0;
+
+boolean is_weapon_on = false;
+boolean is_input_on = false;
+
+unsigned long current_time = 0;
+
+// servo for controlling the power
+Servo power_servo;
+
+// current state position
+unsigned int current_state = INITIAL;
+
+unsigned int current_servo_position = 0;
 
 
 void setup() {
-    digitalWrite(OUTPUT_PIN_IGNITION, LOW);
-    analogWrite(OUTPUT_PIN_POWER, 0);
-
+    pinMode(OUTPUT_SAFETY, OUTPUT);
     pinMode(OUTPUT_PIN_IGNITION, OUTPUT);
-    pinMode(OUTPUT_PIN_POWER, OUTPUT);
+    power_servo.attach(OUTPUT_PIN_SERVO);
 
-    attachInterrupt(digitalPinToInterrupt(PWM_PIN_IN_IGNITION), calc_ignition_pwm, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(PWM_PIN_IN_POWER), calc_power_pwm, CHANGE);
+    digitalWrite(OUTPUT_SAFETY, LOW);
+    digitalWrite(OUTPUT_PIN_IGNITION, LOW);
+    power_servo.write(0);
+
+    attachInterrupt(digitalPinToInterrupt(PWM_PIN_SAFETY), calc_safety, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PWM_PIN_INPUT), calc_input, CHANGE);
 }
 
 
 void loop() {
     current_time = micros();
-    if (pwm_in_ignition_new_throttle_signal) {
-        if(in_boundary(ignition_throttle_len, LOW_LOW_BOUNDARY, LOW_HIGH_BOUNDARY && !was_ignition_pin_low)) {
-            was_ignition_pin_low = true;
-            digitalWrite(OUTPUT_PIN_IGNITION, LOW);
-        } else if (in_boundary(ignition_throttle_len, HIGH_LOW_BOUNDARY, HIGH_HIGH_BOUNDARY) && was_ignition_pin_low) {
-            was_ignition_pin_low = false;
-            digitalWrite(OUTPUT_PIN_IGNITION, HIGH);
-        }
-
-        pwm_in_ignition_new_throttle_signal = false;
-    }
-
-    if(pwm_in_power_new_throttle_signal) {
-        if(power_throttle_len >= LOW_HIGH_BOUNDARY) {
-            int power_value = map(power_throttle_len, LOW_HIGH_BOUNDARY, HIGH_HIGH_BOUNDARY, 0, 255);
-            analogWrite(OUTPUT_PIN_POWER, power_value);
+    if (pwm_safety_throttle_signal) {
+        if(safety_throttle_len >= HIGH_LOW_BOUNDARY) {
+            is_weapon_on = true;
         } else {
-            analogWrite(OUTPUT_PIN_POWER, 0);
+            is_weapon_on = false;
         }
-      pwm_in_power_new_throttle_signal = false;
+
+        pwm_safety_throttle_signal = false;
     }
 
-    if(
-        current_time - last_signal_power_time > NO_SIGNAL_SAFETY_TIMEOUT
-        or current_time - last_signal_ignition_time > NO_SIGNAL_SAFETY_TIMEOUT
-    ) {
-      analogWrite(OUTPUT_PIN_POWER, 0);
-      digitalWrite(OUTPUT_PIN_IGNITION, LOW);
+    if (pwm_input_throttle_signal) {
+        if(input_throttle_len >= HIGH_LOW_BOUNDARY) {
+            is_input_on = true;
+        } else {
+            is_input_on = false;
+        }
+
+        pwm_input_throttle_signal = false;
+    }
+
+    if((current_state == INITIAL) && is_weapon_on) {
+        to_ready();
+    } else if((current_state == READY) && !is_weapon_on) {
+        to_initial();
+    } else if((current_state == READY) && is_input_on) {
+        start_ignition();
+    } else if((current_state == IGNITION) && !is_input_on) {
+        to_ready();
+    } else if((current_state == MOVING_UP_FAST) && is_input_on) {
+        step_up();
+    } else if((current_state == MOVING_UP_FAST) && !is_input_on) {
+        to_ready();
+    } else if((current_state == MOVING_DOWN_SLOW) && is_input_on) {
+        step_down();
+    } else if((current_state == MOVING_DOWN_SLOW) && !is_input_on) {
+        to_ready();
     }
 }
 
 
-void calc_ignition_pwm() {
+void step_up() {
+    if (current_servo_position <= FLAME_SERVO_END) {
+        current_servo_position += FLAME_SERVO_UP_STEP;
+        power_servo.write(current_servo_position);
+        delay(FLAME_SERVO_UP_DELAY);
+    } else {
+        current_state = MOVING_DOWN_SLOW;
+    }
+}
+
+
+void step_down() {
+    if(current_servo_position >= FLAME_SERVO_START) {
+        current_servo_position -= FLAME_SERVO_DOWN_STEP;
+        power_servo.write(current_servo_position);
+        delay(FLAME_SERVO_DOWN_DELAY);
+    } else {
+        current_state = MOVING_UP_FAST;
+    }
+}
+
+
+void to_initial() {
+    current_servo_position = 0;
+    current_state == INITIAL;
+    power_servo.write(current_servo_position);
+    digitalWrite(OUTPUT_PIN_IGNITION, LOW);
+    digitalWrite(OUTPUT_SAFETY, LOW);
+}
+
+
+void to_ready() {
+    current_servo_position = 0;
+    current_state = READY;
+    power_servo.write(current_servo_position);
+    digitalWrite(OUTPUT_PIN_IGNITION, LOW);
+    digitalWrite(OUTPUT_SAFETY, HIGH);
+}
+
+
+void start_ignition() {
+    current_state = IGNITION;
+    current_servo_position = IGNITION_SERVO_POSITION;
+    digitalWrite(OUTPUT_PIN_IGNITION, HIGH);
+    power_servo.write(current_servo_position);
+    delay(IGNITION_DELAY);
+    current_state = MOVING_UP_FAST;
+    digitalWrite(OUTPUT_PIN_IGNITION, LOW);
+}
+
+
+void calc_safety() {
     long current_micros = micros();
 
-    if (digitalRead(PWM_PIN_IN_IGNITION) == HIGH) {
-        pwm_in_ignition_start = current_micros;
+    if (digitalRead(PWM_PIN_SAFETY) == HIGH) {
+        pwm_safety_start = current_micros;
     } else {
-        if (pwm_in_ignition_start && (pwm_in_ignition_new_throttle_signal == false)) {
-            ignition_throttle_len = (int)(current_micros - pwm_in_ignition_start);
-            pwm_in_ignition_start = 0;
+        if (pwm_safety_start && (pwm_safety_throttle_signal == false)) {
+            safety_throttle_len = (int)(current_micros - pwm_safety_start);
+            pwm_safety_start = 0;
 
-            pwm_in_ignition_new_throttle_signal = true;
-            last_signal_ignition_time = current_micros;
+            pwm_safety_throttle_signal = true;
+            last_signal_safety_time = current_micros;
         }
     }
 }
 
 
-void calc_power_pwm() {
+void calc_input() {
     long current_micros = micros();
 
-    if (digitalRead(PWM_PIN_IN_POWER) == HIGH) {
-        pwm_in_power_start = current_micros;
+    if (digitalRead(PWM_PIN_INPUT) == HIGH) {
+        pwm_input_start = current_micros;
     } else {
-        if (pwm_in_power_start && (pwm_in_power_new_throttle_signal == false)) {
-            power_throttle_len = (int)(current_micros - pwm_in_power_start);
-            pwm_in_power_start = 0;
+        if (pwm_input_start && (pwm_input_throttle_signal == false)) {
+            input_throttle_len = (int)(current_micros - pwm_input_start);
+            pwm_input_start = 0;
 
-            pwm_in_power_new_throttle_signal = true;
-            last_signal_power_time = current_micros;
+            pwm_input_throttle_signal = true;
+            last_signal_input_time = current_micros;
         }
     }
 }
